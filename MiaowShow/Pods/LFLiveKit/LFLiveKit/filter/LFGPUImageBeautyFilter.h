bcdf90011bb808be7e62163516515fc4c81a1ae7
dingwenkai <dingwenkai@huami.com>
2024-05-17 15:07:19 +0800

    build |> 更新LFLiveKit到2.6

--- MiaowShow/Pods/LFLiveKit/LFLiveKit/filter/LFGPUImageBeautyFilter.h
+++ MiaowShow/Pods/LFLiveKit/LFLiveKit/filter/LFGPUImageBeautyFilter.h
@@ -1,8 +1,13 @@
-#import "GPUImageFilter.h"
+#if __has_include(<GPUImage/GPUImageFramework.h>)
+#import <GPUImage/GPUImageFramework.h>
+#else
+#import "GPUImage.h"
+#endif
 
 @interface LFGPUImageBeautyFilter : GPUImageFilter {
 }
 
-@property (nonatomic, assign) NSInteger beautyLevel;
-
+@property (nonatomic, assign) CGFloat beautyLevel;
+@property (nonatomic, assign) CGFloat brightLevel;
+@property (nonatomic, assign) CGFloat toneLevel;
 @end
